43779dabee0362db96cd0387e76a27868d699c83
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-21 13:22:14 +0200

    feat: add login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import PageNav from "./../components/PageNavComponent/PageNav";
+import "./../index.css";
+
+import { Form } from "../components/Form";
+export default function LoginPage() {
+  return (
+    <main className="main">
+      <PageNav />
+      <Form />
+    </main>
+  );
+}
+
+
