5b17b1d3eb242885b54576f2787743f1422dbbf0
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-23 23:00:44 +0200

    chore: add local data without fetch

--- src/Hooks/useCities.jsx
+++ src/Hooks/useCities.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+import cities2 from "./../data/cities.json";
 export function useCities() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -9,9 +9,11 @@ export function useCities() {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const res = await fetch("http://localhost:9000/cities");
-        if (!res.ok) throw new Error("failed to fetch");
-        const data = await res.json();
+        // const res = await fetch("http://localhost:9000/cities");
+        // if (!res.ok) throw new Error("failed to fetch");
+        // const data = await res.json();
+        const data = cities2.cities;
+        
         setCities(data);
         console.log(data);
       } catch (err) {
