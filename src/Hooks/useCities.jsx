577f9d50ad804d98c2e87f4bf7bd46bad11d1c8d
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-23 22:41:02 +0200

    feat: fetch cities from local api

---
+++ src/Hooks/useCities.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function useCities() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("http://localhost:9000/cities");
+        if (!res.ok) throw new Error("failed to fetch");
+        const data = await res.json();
+        setCities(data);
+        console.log(data);
+      } catch (err) {
+        setIsError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  function handleDeleteCity(id) {
+    setCities((cities) => cities.filter((city) => city.id !== id));
+  }
+  return { cities, isLoading, isError, handleDeleteCity };
+}
