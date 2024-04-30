ca8e0ff128e593501a5cc0dd531c37236e38d310
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-30 14:42:55 +0300

    feat(app): update routing in App.js
    - remove probs
    - wrap with citiesProvider that contain all State

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import PricingPage from "./pages/PricingPage.jsx";
@@ -7,17 +7,16 @@ import LoginPage from "./pages/LoginPage.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
 
-import { useCities } from "./Hooks/useCities.jsx";
+import { CitiesProvider } from "./contexts/CitiesContext.jsx";
 import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
 // import PageNav from "./components/PageNav";
 function App() {
-  const { cities, isLoading, isError, handleDeleteCity } = useCities();
-
   return (
-    <>
+    <CitiesProvider>
       {/* <PageNav /> */}
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -52,13 +51,15 @@ function App() {
               path="countries"
               element={<CountryList countries={cities} />}
             />
+            <Route index path="cities" element={<CityList />} />
+            <Route path="countries" element={<CountryList />} />
             <Route path="cities/:id" element={<City />} />
             <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<h1>Page not found</h1>} />
         </Routes>
       </BrowserRouter>
-    </>
+    </CitiesProvider>
   );
 }
 
