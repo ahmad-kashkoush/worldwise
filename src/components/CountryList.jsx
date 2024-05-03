4288f547238a37d79c70a25ca45a456bf7c57b83
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-03 07:59:49 +0300

    feat(CountryList): make list dynamic
    on every added new country, list will be updated

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,24 +1,24 @@
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import CountryItem from "./CountryItem";
 import style from "./CountryList.module.css";
 export default function CountryList() {
-  // const x = new Set();
-  // const derivedCountries = [];
-  // countries.forEach((country) => {
-  //   if (x.has(country.country)) return;
-  //   x.add(country.country);
-  //   derivedCountries.push(country);
-  // });
-  const { cities: countries } = useCities();
 
-  const derivedCountries = countries.reduce((arr, city) => {
-    if (!arr.map((el) => el.country).includes(city.country))
-      return [...arr, city];
-    return arr;
-  }, []);
+  const { cities } = useCities();
+
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    setCountries(
+      cities.reduce((arr, city) => {
+        if (!arr.map((el) => el.country).includes(city.country))
+          return [...arr, city];
+        return arr;
+      }, [])
+    );
+  }, [cities]);
   return (
     <ul className={style.countryList}>
-      {derivedCountries.map((country) => (
+      {countries.map((country) => (
         <CountryItem key={country.id} country={country} />
       ))}
     </ul>
