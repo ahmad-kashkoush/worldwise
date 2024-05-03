ed1a8ec6dca68bf1c6a25554bee03624a36f4b50
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-03 07:46:33 +0300

    fix: replace Error component with message
    - "Error" is a javascript keyword, so it can't be used as component name
    - remove Error + repeated files

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import CityItem from "./CityItem";
 import style from "./CityList.module.css";
-import Error from "./Error";
 import Spinner from "./Spinner";
 import Message from "./Message";
 import { useCities } from "../contexts/CitiesContext";
@@ -13,7 +12,7 @@ export default function CityList() {
     return <Message message={"add your first city by clicking on the map"} />;
   if (isError) {
     // console.log(isError);
-    return <Error message={isError} />;
+    return <Message message={isError} />;
   }
   return (
     <ul className={style.cityList}>
