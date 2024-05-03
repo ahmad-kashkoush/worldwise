0a96f753ebeb3b076b47f2275a28fa896446e18f
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-03 07:57:01 +0300

    feat(useGeolocation): create hook
    a hook used to get current position

---
+++ src/hooks/useGeolocation.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+/**
+ * @return {Object} Object containing position, isLoading state, error message, and getCurrentPosition function
+ */
+export function useGeolocation(defaultPosition=null) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [position, setPosition] = useState(defaultPosition);
+
+  function getCurrentPosition() {
+    if (!navigator.geolocation)
+      return setError("browser doesn't support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+        setIsLoading(false);
+        console.log(pos.coords);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+
+    );
+  }
+
+  return { position, isLoading, error, getCurrentPosition };
+}
