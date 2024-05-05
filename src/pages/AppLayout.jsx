e86e5a66cf6dbd7f8dd709ae44d181caa2f5a2c7
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-05 13:58:27 +0300

    feat(logout): implement feature

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,14 @@
 import Map from "../components/Map";
+import User from "../components/User";
 import Sidebar from "./../components/Sidebar";
 import style from "./AppLayout.module.css";
 export default function AppLayout() {
+  
   return (
     <main className={` ${style.app} main`}>
       <Sidebar />
       <Map />
+      <User />
     </main>
   );
 }
