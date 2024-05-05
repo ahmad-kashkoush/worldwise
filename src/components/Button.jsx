e8c57010cdaaf95e12069e0762ff11eae2d6dd99
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-05 13:54:28 +0300

    feat(Button): add classes prob

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import style from "./Button.module.css";
 
-export default function Button({ children, type, buttonType = "", onClick }) {
+export default function Button({ children, type, buttonType = "", onClick, classes }) {
   return (
     <button
       type={buttonType}
       onClick={onClick}
-      className={`${style[type]} ${style.btn}`}
+      className={`${style[type]} ${style.btn} ${classes}`}
     >
       {children}
     </button>
